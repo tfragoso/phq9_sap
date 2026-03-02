0b2f3f10013e7b21b91182a5f2d989abb071f2aa
Tiago Fragoso <tiago.demirandafragoso@unibe.ch>
2026-03-02 16:25:47 +0100

    renv and package setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# phq9_sap
+# Psychometric Validation and Statistical Analysis Plan (SAP) for the Patient Health Questionnaire 9 (PHQ-9)
+
+Desc desc etc.
---
+++ src/00_InstallPackages.R
@@ -0,0 +1,8 @@
+#Packages to be used in the project
+.packages <- c("tidyverse",
+               "admiral",
+               "psych","mirt","lavaan",
+               "rtables","gtsummary",
+               "quarto")
+
+install.packages(.packages)
